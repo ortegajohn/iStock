73137c4e3835c48dbe7c4715229dbed8a81e6efc
Goku <johnortega@bellsouth.net>
2019-05-31 18:42:39 -0400

    heroku2

--- database/database_keys.js
+++ database/database_keys.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 module.exports = {
     keys: {
-        port:       8889,
+        port:       3306,
         host:       process.env.DB_HOST,
         user:       process.env.DB_USER,
         password:   process.env.DB_PASS,
