3cce2efcf6cb138b1bb88e57e79c626dc311bf63
Goku <johnortega@bellsouth.net>
2019-06-01 15:38:39 -0400

    protect db connection with dotenv

--- config/connection.js
+++ config/connection.js
@@ -2,64 +2,25 @@
 // CONNECTION.JS - THIS FILE INITIATES THE CONNECTION TO MYSQL
 // *********************************************************************************
 
-// Dependencies
-// var Sequelize = require("sequelize");
-
-// Creates mySQL connection using Sequelize, the empty string in the third argument spot is our password.
-
-// var sequelize = new Sequelize("test_db", "root", "root", {
-//   host: "localhost",
-//   port: 8889,
-//   dialect: "mysql",
-//   pool: {
-//     max: 5,
-//     min: 0,
-//     idle: 10000
-//   }
-// });
-
-
-// Exports the connection for other files to use
-// module.exports = sequelize;
-
-// const mysql = require( 'mysql' );
-// var connection;
-
-// if(process.env.JAWSDB_URL){
-//   connection = mysql.createConnection(process.env.JAWSDB_URL)
-// }else{
-//   connection = mysql.createConnection({
-//     host: "localhost",
-//     user: "root",
-//     password: "root",
-//     database: "istock_db"
-//   });
-
-// }
-
-// // connection.connect();
-
-// module.exports = connection;
-
-// Set up MySQL connection.
+require('dotenv').config();
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "root",
-  database: "istock_db"
-});
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "root",
+//   database: "istock_db"
+// });
 
 if(process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-    host: "wvulqmhjj9tbtc1w.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    user: "pqks3fbu1rbrezux",
-    password: "wa6acr2zj591tcg5",
-    database: "tvpmjxam28p9sez3"
+    host: process.env.USER_HOST,
+    user: process.env.USER_NAME,
+    password: process.env.USER_PASS,
+    database: process.env.USER_DB
   })
 }
 
